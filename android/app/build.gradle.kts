05646361f9bd5da5f8733a32aec3dafafb5e9bbd
Mirza Akash Kabir <nashim564@gmail.com>
2026-07-23 00:58:16 +0530

    Package named and App label named

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.edumate"
+    namespace = "com.kiazzen.edumate"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.edumate"
+        applicationId = "com.kiazzen.edumate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/kiazzen/edumate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kiazzen.edumate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
